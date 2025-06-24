30202c56e6c34d8e5665af9572a79eb5a836e122
Caio Encarnação <cencarnacaoq@gmail.com>
2025-06-23 21:17:51 -0300

    feat: add more exclusions in the script

--- DownloadSubtitles.ps1
+++ DownloadSubtitles.ps1
@@ -1,4 +1,7 @@
 # Honestly I barely wrote this script. This is a mix of Github Copilot and I'm trying to learn how to use it.
+
+#It's been a few hours since the statement above and I actually had to write another script so this little program could work properly which broadened my understanding of how to use PowerShell. Still a little raw but I'm starting to get the hang of it.
+
 # I need to separate the scriptFolder from the episodeFolder because I need to run the Node.js script from a specific location
 # to use the .env file correctly.
 param(
@@ -7,10 +10,10 @@ param(
 
 $scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-# The removal of 'Bonus' from the directory listing is specific for my Doctor Who episodes
+# All the removals from the directory listing is specific for my Doctor Who episodes
 # Adapt the condition as needed for your use case
 Get-ChildItem -Path $episodeFolder | Where-Object {
-  $_.Name -notmatch "(Bonus|Outro|Intro)" -and $_.Name -match "\.(avi|mkv|mp4)$"
+  $_.Name -notmatch "(Bonus|Outro|Intro|BBC Recon|animation|BW)" -and $_.Name -match "\.(avi|mkv|mp4)$"
 } | ForEach-Object { 
   $filePath = Join-Path $episodeFolder $_.Name
 
